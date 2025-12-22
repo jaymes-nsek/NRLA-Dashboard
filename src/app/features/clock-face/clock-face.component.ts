b1a514386495bedae5e14701b8b9aeafa88a521d
jaymes.nsek <jaymes.nsek@gmail.com>
2025-12-22 15:45:58 +0100

    Animate seconds hand with real-time update

--- src/app/features/clock-face/clock-face.component.ts
+++ src/app/features/clock-face/clock-face.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'nrla-clock-face',
@@ -7,4 +7,43 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './clock-face.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ClockFaceComponent {}
+export class ClockFaceComponent implements OnInit {
+
+  secsHandTransform = '';
+
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+
+  ngOnInit() {
+    this.startAnimation((t: number) => {
+      const d = new Date(t);
+      const ms = d.getMilliseconds();
+      const s = d.getSeconds() + ms / 1000;
+      const secondsAngle = (s / 60) * 360;
+
+      this.secsHandTransform = `rotate(${secondsAngle}deg)`;
+      this.cdr.detectChanges();
+    });
+  }
+
+  /**
+   * Get absolute system time now, no accumulation.
+   */
+  private static nowMs() {
+    const wallClockStart = Date.now();
+    const perfStart = performance.now();
+
+    return wallClockStart + (performance.now() - perfStart);
+  }
+
+  private startAnimation(render: any) {
+    function frame() {
+      const t = ClockFaceComponent.nowMs();
+      render(t);
+      requestAnimationFrame(frame);
+    }
+
+    requestAnimationFrame(frame);
+  }
+}
